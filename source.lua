be01b75f0159fdc177d0c7a1d5a9b991412e9e80
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2023-06-03 16:58:37 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "Daniel's Cursed BSS",
