0f5b77cb83908249ec5a770e0d41ac8079cca60f
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2023-06-03 15:52:06 -0400

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,56 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Daniel's Cursed BSS",
+    LoadingTitle = "Daniel's Cursed BSS",
+    LoadingSubtitle = "Join our discord server and stay updated about an update or new key",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = "DanielCursedBSS", -- Create a custom folder for your hub/game
+       FileName = "DanCursedBSS"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+    KeySystem = true, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Daniel's Cursed BSS key system",
+       Subtitle = "well its obvious.",
+       Note = "Join the discord server to get the key!",
+       FileName = "DCBK", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"https://raw.githubusercontent.com/danielgamer9799/DanielCursedBSS/main/key.lua"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+ })
+
+ Rayfield:Notify({
+    Title = "Hello there!",
+    Content = "welcome to Daniel's Cursed BSS! hopefully you have fun!",
+    Duration = 6.5,
+    Image = 5101329469,
+    Actions = { -- Notification Buttons
+       Ignore = {
+          Name = "thanks :)",
+          Callback = function()
+          print("NATRO SO BROKE")
+       end
+    },
+ },
+ })
+ 
+ 
+
+ local CursedTab = Window:CreateTab("Cursed", 12854797313) -- Title, Image
+ local LovesSection = CursedTab:CreateSection("🥰[insert] loves [insert]🥰")
+
+ local LovesInput = CursedTab:CreateInput({
+    Name = "Insert stuff",
+    PlaceholderText = "Buoyant Bee Loves Fuzzy Bee!",
+    RemoveTextAfterFocusLost = false,
+    Callback = function(Text)
+        
+    end,
+ })
